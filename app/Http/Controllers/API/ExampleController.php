94dcb8ab5d701fd76b1e8308e1e4966c4acb87b9
Mohammed Che <mocherbatji@gmail.com>
2024-01-10 17:21:07 +0000

    Added API example route and controller

---
+++ app/Http/Controllers/API/ExampleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class ExampleController extends Controller
+{
+    public function index() {
+
+        return response()->json([
+            'message' => "All good.",
+            'data' => Book::all()
+        ], 200);
+
+    }
+}
